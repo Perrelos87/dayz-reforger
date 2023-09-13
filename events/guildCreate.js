57cc5d3dfc99dd68524f311318f0ef786e8bd9c0
Perrelos87 <86109156+Perrelos87@users.noreply.github.com>
2023-09-14 01:23:07 +0200

    Update guildCreate.js
    create category and channels when joining a discord

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,3 +1,42 @@
-module.exports = (client, guild) => {
-  require("../util/RegisterSlashCommands").RegisterGuildCommands(client, guild.id);
+const { GuildChannelManager } = require("discord.js");
+
+module.exports = async (client, guild) => {
+  try {
+    // Registra los comandos de Slash en el servidor
+    await require("../util/RegisterSlashCommands").RegisterGuildCommands(client, guild.id);
+
+    const category = await guild.channels.create({
+      name: "📋 𝐓𝐡𝐞 𝐋𝐚𝐬𝐭 𝐃𝐞𝐚𝐭𝐡 𝐟𝐞𝐞𝐝𝐬",
+      type: "4",
+      reason: "Creación de categoría para feeds",
+    });
+
+    const categoryId = category.id;
+
+    const createChannel = async (name) => {
+      await guild.channels.create({
+        name: name,
+        type: '0',
+        parent: categoryId,
+        permissionOverwrites: [{
+          id: guild.roles.everyone.id,
+          allow: ['1024', '2048', '8192'],
+          deny: ['8']
+        }]
+      });
+    }
+
+    await createChannel("➖》online");
+    await createChannel("➖》🚪-conexion");
+    await createChannel("➖》💀-killfeed");
+    await createChannel("➖》📡-gps");
+    await createChannel("➖》👀-hitsfeed");
+    await createChannel("➖》☠-Pvefeed");
+    await createChannel("➖》🔨-Buildfeed");
+
+    console.log("Canales y carpeta creados correctamente.");
+
+  } catch (error) {
+    console.error("Error al crear los canales y carpeta:", error);
+  }
 };
